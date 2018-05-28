1adeba44cc1798c4119e58d642d09a1a801178fb
Tim Foerster <tim.foerster@hetzner.com>
2018-05-29 01:32:55 +0200

    Fix git merge
    Signed-off-by: Tim Foerster <tim.foerster@hetzner.com>

--- lib/battlesnake/snake.rb
+++ lib/battlesnake/snake.rb
@@ -39,72 +39,61 @@ module Battlesnake
       valid_moves.each do |move|
         moves = [move]
         pos = snake_head.move(move)
+        next if pos != to && world.blocked(pos, ignore: ignore)
         points = [pos]
-        until (pos - to).magnitude == 0
-          ret = valid_moves(last_move: moves.last).map do |m| 
+        until (pos - to).magnitude.zero?
+          ret = valid_moves(last_move: moves.last).map do |m|
             a = [m, pos.move(m)]
             points << a[1]
             a << (a[1]- to).magnitude
           end
           ret.sort_by!{|x| x[2]}
-          ret.delete_if do |x|
-            world.blocked(x[1]) || points.count(x[1]) > 1
-          end
-          next if pos != to && world.blocked(pos, ignore: ignore)
-          points = [pos]
-          until (pos - to).magnitude.zero?
-            ret = valid_moves(last_move: moves.last).map do |m| 
-              a = [m, pos.move(m)]
-              points << a[1]
-              a << (a[1]- to).magnitude
+          unless ret.first[1] == to
+            ret.delete_if do |x|
+              world.blocked(x[1], ignore: ignore) || points.count(x[1]) > 1
             end
-            ret.sort_by!{|x| x[2]}
-            unless ret.first[1] == to
-              ret.delete_if do |x|
-                world.blocked(x[1], ignore: ignore) || points.count(x[1]) > 1
-              end
-            end
-            if ret.first.nil?
-              moves << :unresolved
-              possibilities << moves
-              break
-            end
-            moves << ret.first[0]
-            pos = ret.first[1]
           end
-          possibilities << moves if pos == to
-        end
-        possibilities
-      end
-
-      def move(ignore: nil)
-        targets = world.targets.map do |f|
-          case f
-          when Food 
-            walk(to: f.pos, ignore: ignore)
+          if ret.first.nil?
+            moves << :unresolved
+            possibilities << moves
+            break
           end
-        end.compact.flatten(1)
-        x = targets.reject{|x| x.last == :unresolved }
-        a = nil
-        if x.empty?
-          a = p(targets.sort_by{|f| f.size}.reverse)
-        else
-          a = p(x.sort_by{|f| f.size})
+          moves << ret.first[0]
+          pos = ret.first[1]
         end
-        if a.empty? and ignore.nil?
-          return	      move(ignore: :possible_next_head)
+        possibilities << moves if pos == to
+      end
+      possibilities
+    end
+
+    def move(ignore: nil)
+      targets = world.targets.map do |f|
+        case f
+        when Food 
+          walk(to: f.pos, ignore: ignore)
         end
-        a.find{|x| b = world.blocked(head.move(x[0])); b.nil? || b == :food }&.first || a[0][0]
+      end.compact.flatten(1)
+      x = targets.reject{|x| x.last == :unresolved }
+      a = nil
+      if x.empty?
+        a = p(targets.sort_by{|f| f.size}.reverse)
+      else
+        a = p(x.sort_by{|f| f.size})
       end
+      if a.empty? and ignore.nil?
+        return        move(ignore: :possible_next_head)
+      end
+      a.find{|x| b = world.blocked(head.move(x[0])); b.nil? || b == :food }&.first || a[0][0]
+    end
 
-      def update(health:, points:)
-        @health = health
-        @points_before = @points.dup
-        @points = points
-      end
+    def update(health:, points:)
+      @health = health
+      @points_before = @points.dup
+      @points = points
+    end
 
-      def size
-        points.size
-      end
+    def size
+      points.size
     end
   end
+end
